35ef8d2eb0b47de3a23174ac86787513d2bfc500
Maaz Khan <maaz.khan25925@gmail.com>
2023-10-29 03:14:44 +0530

    Feat: Selecting a column via dropdown from CSV Used streamlit's selectbox component Added cleantext library

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import pandas as pd
 from textblob import TextBlob
+import cleantext
 
 def analyze_sentiment(text):
-  analysis = TextBlob(text)
+  text = cleantext.clean(text=text, clean_all=True)
+  analysis = TextBlob(text) # can add classifier by loading one from another file.
   sentiment_polarity = analysis.sentiment.polarity
 
   if sentiment_polarity > 0:
@@ -15,18 +17,26 @@ def analyze_sentiment(text):
 
 st.title("Sentiment Analysis App")
 
-uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
+uploaded_file = st.file_uploader(label="Upload a CSV file", type=["csv"])
 
 if uploaded_file is not None:
   df = pd.read_csv(uploaded_file)
-  df['Sentiment'] = df['text'].apply(analyze_sentiment)
+  # content_column = st.text_input(label="Enter column name to analyse", value="text")
+  content_column = st.selectbox(label="Select a column to analyse", options=df.columns, index=None)
+  try:
+    df["Sentiment"] = df[content_column].apply(analyze_sentiment)
+  except KeyError as e:
+    print(e.with_traceback)
+  except TypeError as e:
+    print(e.with_traceback)
+    st.write("Non-textual data in selected column")
   st.subheader("Result")
   st.write(df)
-  # st.dataframe(df)
 
-  sentiment_counts = df['Sentiment'].value_counts()
-  st.subheader("Sentiment Distribution")
-  st.bar_chart(sentiment_counts)
+  if "Sentiment" in df.columns:
+    sentiment_counts = df["Sentiment"].value_counts()
+    st.subheader("Sentiment Distribution")
+    st.bar_chart(sentiment_counts)
 
 st.subheader("Manual Sentiment Analysis")
 user_input = st.text_area("Enter text: ")
